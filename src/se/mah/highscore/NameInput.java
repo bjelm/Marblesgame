982b0750a30d0824650a1764743946bb3701676f
Erika Bergqvist <eriiika92@hotmail.com>
2013-05-30 13:43:07 +0200

    plocka bort lite skräp

--- src/se/mah/highscore/NameInput.java
+++ src/se/mah/highscore/NameInput.java
@@ -3,9 +3,6 @@ package se.mah.highscore;
 
 import se.mah.marbles.R;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
