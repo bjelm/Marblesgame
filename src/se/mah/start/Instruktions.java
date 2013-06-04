91c4a31ccfb136d9ba7af88b9dc6ba752ebc7d57
Mark Wibom <wibom.mark@gmail.com>
2013-06-04 16:11:16 +0200

    ny clas

---
+++ src/se/mah/start/Instruktions.java
@@ -0,0 +1,17 @@
+package se.mah.start;
+
+import se.mah.marbles.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Instruktions extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.instruktions);
+	}
+	
+
+}
